05b28410314ccde1d970a89264e287409c423c57
igor <85964276+igcardoso@users.noreply.github.com>
2024-03-24 08:14:16 -0300

    update src/app/javascript/loading.js

--- src/app/javascript/loading.js
+++ src/app/javascript/loading.js
@@ -1,8 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#pages-and-header').style.display = 'none';
-  setTimeout(function() {
-    document.querySelector('#loading-app p').classList.add('netflix-effect');
-  }, 700);
+  
 	setTimeout(function() {
 		document.querySelector('#loading-app').style.display = 'none';
 		document.querySelector('#pages-and-header').style.display = 'block';
