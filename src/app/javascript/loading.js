f46f06e47e2af6073331c491178c8e21bf706617
igor <85964276+igcardoso@users.noreply.github.com>
2024-03-24 08:25:27 -0300

    update src/app/javascript/loading.js

--- src/app/javascript/loading.js
+++ src/app/javascript/loading.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#pages-and-header').style.display = 'none';
   setTimeout(function() {
     document.querySelector('#loading-app .intro-static').classList.add('active');
-    document.querySelector('#loading-app .intro-static').classList.add('hidden');
+    document.querySelector('#loading-app .intro-animated').classList.remove('active');
   }, 200);
 	setTimeout(function() {
 		document.querySelector('#loading-app').style.display = 'none';
