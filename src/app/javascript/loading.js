db60b57533550023873dbe1ac3141a0efa459122
igor <85964276+igcardoso@users.noreply.github.com>
2024-03-24 08:18:36 -0300

    update src/app/javascript/loading.js

--- src/app/javascript/loading.js
+++ src/app/javascript/loading.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#pages-and-header').style.display = 'none';
   setTimeout(function() {
     document.querySelector('#loading-app img').src = '../../../accets/app-icons/Logonetflix.png';
-  }, 700);
+  }, 100);
 	setTimeout(function() {
 		document.querySelector('#loading-app').style.display = 'none';
 		document.querySelector('#pages-and-header').style.display = 'block';
